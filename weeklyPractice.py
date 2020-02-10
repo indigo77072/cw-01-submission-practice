9c4e73399fb9b7a4b6d5e7119a639aa715322f1c
Indigo Emanuel <indigo.emanue@gmail.com>
2020-02-10 13:33:22 -0500

    currently practicing hw-02-programming-fundamentals

--- weeklyPractice.py
+++ weeklyPractice.py
@@ -1,1 +1,8 @@
 print("Whats your name")
+userName = input()
+print(f"Your name is {userName} ")
+
+print("How old are you?")
+age = input()
+bYear = int(2020) - int(age)
+print(f'Your name is {userName} you are {age} years old, and you were born in {bYear}')
